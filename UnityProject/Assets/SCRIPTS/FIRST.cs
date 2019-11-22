92f7c53cebcdbeafb59fbeade3e25ff8503616e8
yjpeng19910216 <57255550+yjpeng19910216@users.noreply.github.com>
2019-11-22 21:44:12 +0800

    +if

--- UnityProject/Assets/SCRIPTS/FIRST.cs
+++ UnityProject/Assets/SCRIPTS/FIRST.cs
@@ -1,7 +1,16 @@
 ﻿
-using UnityEngine;
+using UnityEngine;  //引用unity API
 
+//類別 類別名稱 文件名稱
 public class FIRST: MonoBehaviour
 {
-  
+    //{為程式內容}
+    //宣告變數-定義欄位 Field
+    //顯示與否(修飾詞) 欄位內容 欄位名稱 結束
+    //私人 - 隱藏  private (預設)
+    //公開 - 顯示 public
+    public int coin;               //整數
+    public float speed;            //浮點數
+    public string prop;            //字串
+    public bool dead;              //布林值
 }
---
+++ UnityProject/Assets/SCRIPTS/LearnIF.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class LearnIF : MonoBehaviour
+{
+    public int combo;
+
+    private void Start()
+    { 
+     if (combo <=49)
+        {
+            print("攻擊*1");
+        }
+     else if (combo <= 99)
+        {
+            print("攻擊*2");
+        }
+     else if (combo <= 149)
+        {
+            print("攻擊*5");
+        }
+     else if (combo >= 150)
+        {
+            print("攻擊*10");
+        }
+
+    }
+    private void Update()
+    {
+        if (combo <= 49)
+        {
+            print("攻擊*1");
+        }
+        else if (combo <= 99)
+        {
+            print("攻擊*2");
+        }
+        else if (combo <= 149)
+        {
+            print("攻擊*5");
+        }
+        else if (combo >= 150)
+        {
+            print("攻擊*10");
+        }
+    }
+}
